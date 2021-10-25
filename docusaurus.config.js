da7e2f7c1dc05b2e5a6f2acb60ded0d9b323072e
Guus Frenken <g.frenken@qteco.nl>
2021-10-25 11:27:22 +0200

    Initial commit

---
+++ docusaurus.config.js
@@ -0,0 +1,64 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+    title: 'October CMS Docs',
+    tagline: 'Documentatie voor het beheren van October CMS websites opgeleverd door Qteco',
+    url: 'https://docs.qteco.nl',
+    baseUrl: '/',
+    onBrokenLinks: 'throw',
+    onBrokenMarkdownLinks: 'warn',
+    favicon: 'img/favicon.ico',
+    organizationName: 'qteco',
+    projectName: 'octobercms-docs',
+
+    presets: [
+        [
+            '@docusaurus/preset-classic',
+            /** @type {import('@docusaurus/preset-classic').Options} */
+            ({
+                docs: {
+                    routeBasePath: '/',
+                    sidebarPath: require.resolve('./sidebars.js'),
+                    //editUrl: 'https://github.com/qteco/octobercms-docs/edit/main/',
+                },
+                theme: {
+                    customCss: require.resolve('./src/css/custom.css'),
+                },
+            }),
+        ],
+    ],
+
+    themeConfig:
+        /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+        ({
+            navbar: {
+                title: 'October CMS Docs',
+                logo: {
+                    alt: 'My Site Logo',
+                    src: 'img/logo.svg',
+                },
+                items: [
+                    {
+                        href: 'https://github.com/qteco/octobercms-docs',
+                        label: 'GitHub',
+                        position: 'right',
+                    },
+                ],
+            },
+            footer: {
+                style: 'light',
+                copyright: `&copy; ${new Date().getFullYear()} Qteco <a href="https://docusaurus.io" target="_blank">Built with Docusaurus</a>`,
+            },
+            prism: {
+                theme: lightCodeTheme,
+                darkTheme: darkCodeTheme,
+            },
+        }),
+};
+
+module.exports = config;
